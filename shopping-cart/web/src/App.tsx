7cc6f523054b7b79c7b42241ded6938bdf763241
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2023-04-16 11:11:18 -0300

    Creating project layout

--- shopping-cart/web/src/App.tsx
+++ shopping-cart/web/src/App.tsx
@@ -1,10 +1,50 @@
+import Summary from './Components/Summary/Summary'
+import RowProducts from './Components/RowProducts/TableRow'
+
 import { useState } from 'react'
-import './App.css'
+
+import './style.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return <div className="App"></div>
+  return (
+    <>
+      <header className="header">
+        <h1>Carrinho de compras</h1>
+      </header>
+
+      <main>
+        <div className="content">
+          <section>
+            <table>
+              <thead>
+                <tr>
+                  <th>Produto</th>
+                  <th>Preço</th>
+                  <th>Quantidade</th>
+                  <th>Total</th>
+                  <th>-</th>
+                </tr>
+              </thead>
+              <tbody>
+                <RowProducts />
+                <RowProducts />
+                <RowProducts />
+                <RowProducts />
+                <RowProducts />
+                <RowProducts />
+                <RowProducts />
+              </tbody>
+            </table>
+          </section>
+          <aside>
+            <Summary />
+          </aside>
+        </div>
+      </main>
+    </>
+  )
 }
 
 export default App
